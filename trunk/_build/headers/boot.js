b5cc71f735f26ba75c718489d34ea74950ece749
KrocCamen <KrocCamen@49301b06-fe24-0410-8ffa-39b3c36bcedc>
2006-12-24 13:31:53 +0000

    Improvements to the build system, now adds license headers to files

---
+++ trunk/_build/headers/boot.js
@@ -0,0 +1,20 @@
+/* jaxgames - boot.js - go to http://code.google.com/p/jaxgames/ for full source code
+=============================================================================================================================
+jaxgames, jax (c) copyright Kroc Camen 2005, 2006, licensed under the Creative Commons Attribution-ShareAlike 2.5 license
+
+Prototype JavaScript framework, version 1.5.0_rc1 (c) 2005 Sam Stephenson <sam@conio.net>
+Prototype is freely distributable under the terms of an MIT-style license.
+For details, see the Prototype web site: http://prototype.conio.net/
+     
+script.aculo.us scriptaculous.js v1.6.5, Wed Nov 08 14:17:49 CET 2006
+Copyright (c) 2005, 2006 Thomas Fuchs (http://script.aculo.us, http://mir.aculo.us)
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation
+files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy,
+modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the
+Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software
+
+Firebug, Firebug Lite is the work of Joe Hewitt (http://getfirebug.com)
+*/
---
+++ trunk/_build/headers/jaxgames.js
@@ -0,0 +1,4 @@
+/* =======================================================================================================================
+   jax games (c) copyright Kroc Camen 2005, 2006. licensed under the Creative Commons Attribution-ShareAlike 2.5 license
+   ======================================================================================================================= */
+// this is a compressed version, go to http://code.google.com/p/jaxgames/ for full source code
