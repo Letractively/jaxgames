baf55089a13402d780dfaf76ddd68f29436111bd
KrocCamen <KrocCamen@49301b06-fe24-0410-8ffa-39b3c36bcedc>
2007-03-09 11:58:12 +0000

    NEW: New release on Google Code FIX: _build/libs/makeboot.js was incorrectly including firebugx.js, preventing the compiled release working on browsers without Firebug installed :/ FIX: error in `game.end`. `winner` is an object not a boolean ADD: Improved the heads-up to allow a callback function ADD: The heads-up object has been moved from blacjax to shared so that all games can access it via `shared.headsup.show`

--- games/blacjax/index.php
+++ games/blacjax/index.php
@@ -56,9 +56,6 @@
 			        &nbsp;
 			</div>
 		</div>
-		<div id="game-status" class="hv" style="display: none;"><div class="hv1"><div class="hv2">
-			<div id="game-status-bar" class="hvt"><div id="game-status-text"></div></div>
-		</div></div></div>
 	</div>
 	<?php include ("../-/inc/footer.html")?>
 </div></body>
